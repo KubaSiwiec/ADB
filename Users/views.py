47be337bf719a08ed78105ccc9fec78ec2f2c750
Kuba Siwiec <jakubsiwiec.nwm@gmail.com>
2021-09-12 20:59:44 +0200

    Create registration page

--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        print('goog')
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            print('good')
+            # zapisuje formę
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('booking-home')
+    else:
+        print('bad')
+        form = UserRegisterForm()
     return render(request, 'Users/register.html', {'form': form})
