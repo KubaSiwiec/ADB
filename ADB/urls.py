16c4c20c68b025a8fa75fc5044ed45beae82d0f2
Kuba Siwiec <jakubsiwiec.nwm@gmail.com>
2021-09-12 21:37:16 +0200

    Create login, logout and profile pages

--- ADB/urls.py
+++ ADB/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Users import views as User_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('booking/', include('Booking.urls')),
     path('register/', User_views.register, name='register'),
+    path('profile/', User_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
 ]
 
--- Users/views.py
+++ Users/views.py
@@ -12,9 +12,12 @@ def register(request):
             # zapisuje formę
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('booking-home')
+            messages.success(request, f'Account created for {username}! You can no log in')
+            return redirect('register')
     else:
         print('bad')
         form = UserRegisterForm()
     return render(request, 'Users/register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'users/profile.html')
