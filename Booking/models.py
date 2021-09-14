dfbfa3a11a60350aecb809c7b49b3ebe0ac7a95d
Kuba Siwiec <jakubsiwiec.nwm@gmail.com>
2021-09-14 21:52:36 +0200

    Add queries to choose from free seats

--- Booking/models.py
+++ Booking/models.py
@@ -38,6 +38,7 @@ class Seat(models.Model):
 class Movie_seat(models.Model):
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     seat_id = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    busy = models.BooleanField(default=False)
     def __str__(self):
         return str(self.seat_id.name)
 
--- Booking/urls.py
+++ Booking/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MoviesListView, MoviesDetailView, movie_detail_view
+from .views import MoviesListView, movie_detail_view
 from . import views
 
 urlpatterns = [
